737d45dc09216f7b392195302b6ef0dd0a7c35b4
Ryan Desmond <radesmond@gmail.com>
2020-04-16 11:52:32 -0700

    ryan updates

--- src/main/java/com/mutts_app/config/MvcConfig.java
+++ src/main/java/com/mutts_app/config/MvcConfig.java
@@ -1,15 +1,18 @@
 package com.mutts_app.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public abstract class MvcConfig implements WebMvcConfigurer {
+public class MvcConfig extends WebMvcConfigurerAdapter {
 
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("/").setViewName("login");
-//        registry.addViewController("/login").setViewName("login");
-//    }
-
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
 }
--- src/main/java/com/mutts_app/config/SecurityConfig.java
+++ src/main/java/com/mutts_app/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.mutts_app.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 
@@ -12,50 +15,65 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
- 
-//	@Autowired
-//	DataSource dataSource;
- 
-//	@Autowired
-//	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.jdbcAuthentication().dataSource(dataSource)
-//				.usersByUsernameQuery("select user_name, password, enabled from user where user_name=?")
-//				.authoritiesByUsernameQuery("select u.user_name, r.role " +
-//						"from role r " +
-//						"join user_role ur " +
-//						"on r.role_id = ur.role_id " +
-//						"join user u " +
-//						"on u.user_id = ur.user_id " +
-//						"where u.user_name=?");
-//	}
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Value("${spring.queries.users-query}")
+	private String usersQuery;
+
+	@Value("${spring.queries.roles-query}")
+	private String rolesQuery;
 
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-				.withUser("joe")
-				.password("{noop}123")
-				.roles("USER")
-				.and()
-				.withUser("ryan")
-				.password("{noop}123")
-				.roles("USER");
+	protected void configure(AuthenticationManagerBuilder auth)
+			throws Exception {
+		auth.
+				jdbcAuthentication()
+				.usersByUsernameQuery(usersQuery)
+				.authoritiesByUsernameQuery(rolesQuery)
+				.dataSource(dataSource)
+				.passwordEncoder(bCryptPasswordEncoder);
 	}
- 
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers("/", "/home","/users").permitAll()
-//				.antMatchers("/users").hasRole("USER")
-				.anyRequest().authenticated()
-				.and().formLogin().loginPage("/login").permitAll()
-				.defaultSuccessUrl("/index")
-				.and().logout().permitAll();
 
-		http.exceptionHandling().accessDeniedPage("/403");
+		http.
+				authorizeRequests()
+				.antMatchers("/").permitAll()
+				.antMatchers("/login").permitAll()
+				.antMatchers("/registration").permitAll()
+				.antMatchers("/users/**").hasAuthority(("ADMIN"))
+				.antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest().authenticated()
+				.and().csrf().disable().formLogin()
+				.loginPage("/login").failureUrl("/login?error=true")
+				.defaultSuccessUrl("/index")
+				.usernameParameter("email")
+				.passwordParameter("password")
+				.and()
+				.logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+				.logoutSuccessUrl("/").and().exceptionHandling()
+				.accessDeniedPage("/access-denied");
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web
+				.ignoring()
+				.antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
 	}
 }
