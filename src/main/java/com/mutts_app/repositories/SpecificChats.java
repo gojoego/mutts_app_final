bfc5b41d5960745765d6ddddf49bacce191a5dcf
Joe Gallego <josephjgallego@gmail.com>
2020-04-19 15:37:15 -0700

    updating

---
+++ src/main/java/com/mutts_app/repositories/SpecificChats.java
@@ -0,0 +1,67 @@
+package com.mutts_app.repositories;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class SpecificChats {
+
+    String id;
+    String message;
+    String dateSent;
+    String chatId;
+    String senderId;
+    String chatTitle;
+
+    public SpecificChats() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDateSent() {
+        return dateSent;
+    }
+
+    public void setDateSent(String dateSent) {
+        this.dateSent = dateSent;
+    }
+
+    public String getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(String chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getChatTitle() {
+        return chatTitle;
+    }
+
+    public void setChatTitle(String chatTitle) {
+        this.chatTitle = chatTitle;
+    }
+}
