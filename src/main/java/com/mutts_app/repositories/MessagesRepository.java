c96699fe103f59ac3aa2f750ca324dc9f4bc6495
Joe Gallego <josephjgallego@gmail.com>
2020-04-19 15:49:59 -0700

    updating

--- src/main/java/com/mutts_app/repositories/MessagesRepository.java
+++ src/main/java/com/mutts_app/repositories/MessagesRepository.java
@@ -3,4 +3,7 @@ package com.mutts_app.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MessagesRepository extends JpaRepository {
+
+    public Messages findByMessagesId(long messagesId);
+
 }
